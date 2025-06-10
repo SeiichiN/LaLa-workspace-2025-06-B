00d144628410aa38363aee911dfda1e06549995b
SeiichiN <billie175@gmail.com>
2025-06-10 17:06:04 +0900

    move method.

--- game_capsel/src/main/Character.java
+++ game_capsel/src/main/Character.java
@@ -27,6 +27,24 @@ public abstract class Character {
 		System.out.println(m.getName() + "に" + damage + "ポイントのダメージを与えた");
 		
 	}
+	
+	public void move(Board board) {
+		char ch = Util.choice("w↑ s↓ a← d→ > ");
+		switch (ch) {
+		case 'w' -> {
+			y = Math.max(y-1, 0);
+		}
+		case 's' -> {
+			y = Math.min(y+1, board.getYsize()-1);
+		}
+		case 'a' -> {
+			x = Math.max(x-1, 0);
+		}
+		case 'd' -> {
+			x = Math.min(x+1, board.getXsize()-1);
+		}
+		}
+	}
 
 	public int getY() {
 		return y;
