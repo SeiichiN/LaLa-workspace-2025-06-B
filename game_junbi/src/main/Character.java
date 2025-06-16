244f56d3c4c70f2c1b86c129d24edfa1bcb67cf6
SeiichiN <billie175@gmail.com>
2025-06-16 15:13:33 +0900

    Characterクラス useItem()メソッド

---
+++ game_junbi/src/main/Character.java
@@ -0,0 +1,104 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public abstract class Character {
+	int y;
+	int x;
+	int hp;
+	int mp;
+	String name;
+	char suffix;
+	List<Item> itemList;
+	
+	public Character(String name, char suffix) {
+		this.name = name;
+		this.suffix = suffix;
+		this.hp = 100;
+		this.mp = 50;
+		this.itemList = new ArrayList<>();
+	}
+
+	public void setPosition(Board board) {
+		this.y = new Random().nextInt(board.ysize);
+		this.x = new Random().nextInt(board.xsize);
+	}
+	
+	public void attack(Monster m) {
+		System.out.println(this.name + "は" + m.name + "を攻撃した！");
+		int damage = new Random().nextInt(30);
+		m.hp -= damage;
+		System.out.println(m.name + "に" + damage + "ポイントのダメージを与えた");
+		
+	}
+	
+	public void selectAction(Board board) {
+		char ch = Util.choice("w↑ s↓ a← d→ u:使う i:情報 l:見る > ");
+		switch (ch) {
+		case 'u' -> {
+			useItem();
+		}
+		case 'w','s','a','d' -> {
+			move(ch, board);
+		}
+		}
+		
+	}
+	
+	private void useItem() {
+		if (itemList.size() == 0) {
+			System.out.println("まだ何も持ってません");
+			return;
+		}
+		String str = "どれを使いますか？ \n";
+		for (int i = 0; i < itemList.size(); i++) {
+			str += " (" + i + ")" + itemList.get(i).name;
+		}
+		str += " (9)使わない > ";
+		char ch = Util.choice(str);
+		int num = ch - '0';
+		if (num == 9) return;
+		if (itemList.get(num) instanceof Potion p) {
+			this.hp = Math.min(this.hp + p.recovHp, 100);
+			System.out.println(this.name + "のHPが回復した");
+		} else if (itemList.get(num) instanceof Ether e) {
+			this.mp = Math.min(this.mp + e.recovMp, 50);
+			System.out.println(this.name + "のMPが回復した");
+		}
+		itemList.remove(num);
+	}
+	
+	/**
+	 * 拾いますか？ y:拾う n:拾わない
+	 * 自分の List<Item>にそれを加える
+	 * map[アイテムy][アイテムx] を '.' に変える
+	 */
+	public boolean takeItem(Item item) {
+		System.out.println(item.name + "が落ちている！");
+		char ch = Util.choice("拾いますか？ y:拾う n:拾わない > ");
+		if (ch == 'y') {
+			itemList.add(item);
+			return true;
+		}
+		return false;
+	}
+	
+	private void move(char ch, Board board) {
+		switch (ch) {
+		case 'w' -> {
+			y = Math.max(y-1, 0);
+		}
+		case 's' -> {
+			y = Math.min(y+1, board.ysize-1);
+		}
+		case 'a' -> {
+			x = Math.max(x-1, 0);
+		}
+		case 'd' -> {
+			x = Math.min(x+1, board.xsize-1);
+		}
+		}
+	}
+}  // class end
---
+++ game_junbi/src/main/Manager.java
@@ -0,0 +1,55 @@
+package main;
+
+public class Manager {
+	Board board;
+	Goblin goblin;
+	Slime slime;
+	Hero hero;
+	Potion potion;
+	Ether ether;
+	
+	public void start() {
+		init();
+		while (hero.hp > 0) {
+			board.printMap(hero);
+			checkHere();
+			hero.selectAction(board);
+		}
+	}
+	private void checkHere() {
+		char ch = board.map[hero.y][hero.x];
+		switch (ch) {
+		case 'p' -> {
+			if (hero.takeItem(potion)) {
+				board.map[hero.y][hero.x] = '.';
+			}
+		}
+		case 'e' -> {
+			if (hero.takeItem(ether)) {
+				board.map[hero.y][hero.x] = '.';
+			}
+		}
+		case 'g' -> {
+			System.out.println("ゴブリンが現れた");
+		}
+		case 's' -> {
+			System.out.println("スライムが現れた");
+		}
+		}
+	}
+	
+	private void init() {
+		this.board = new Board();
+		board.makeMap();  // マップ作成
+		this.goblin = new Goblin();
+		goblin.setPosition(board);
+		this.slime = new Slime();
+		slime.setPosition(board);
+		this.potion = new Potion();
+		potion.setPosition(board);
+		this.ether = new Ether();
+		ether.setPosition(board);
+		this.hero = new Hero();
+		hero.setPosition(board);
+	}
+}
